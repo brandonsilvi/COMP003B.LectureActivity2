1ef5e475e2bb2b8eca4d3e20d4c43aad590e54a0
Brandon Silvibarr <brandon.silvi@gmail.com>
2026-03-19 07:32:38 -0700

    completed steps up to updating nav

---
+++ Program.cs
@@ -0,0 +1,47 @@
+using COMP003B.LectureActivity2.Middleware;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+
+namespace COMP003B.LectureActivity2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            
+            //Add services to the container
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+            
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                //The default HSTS value is 30 days. You may want to change this for production scenarios.
+                app.UseHsts();
+            }
+            
+            //Middleware sequence
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+
+            app.UseMiddleware<COMP003B.LectureActivity2.Middleware.RequestLoggingMiddleware>();
+
+            app.UseWelcomePage("/welcome");
+
+            app.UseMiddleware<RequestLoggingMiddleware>();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
